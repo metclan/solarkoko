e316cd22f362830102750c7628806182b1793c65
mac <mac@Users-MacBook-Pro.local>
2024-10-06 21:47:58 +0100

    fixed signout button to use getEndPoint function

--- components/signout-button.tsx
+++ components/signout-button.tsx
@@ -1,7 +1,8 @@
 "use client";
+import { getEndPoint } from "@/utils/getEndPoint";
 export function SignoutButton () {
    async function handleLogout () {
-    const response = await fetch('http://localhost:3000/api/auth', { 
+    const response = await fetch(`${getEndPoint()}/api/auth`,{
       method : 'POST',
     })
     if(response.ok){
